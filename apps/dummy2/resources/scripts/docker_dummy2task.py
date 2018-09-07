d2e2918fb934f958c3da085ef8f6f93f691ca6ee
michal.plebanski <michal.plebanski@golem.network>
2018-09-07 15:16:00 +0200

    Add dummy2 password breaking example

---
+++ apps/dummy2/resources/scripts/docker_dummy2task.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+import imp
+import os
+
+import params  # This module is generated before this script is run
+
+
+def run(data_files, subtask_data, difficulty, result_size, result_file):
+    code_file = os.path.join(params.RESOURCES_DIR, "code", "computing.py")
+    computing = imp.load_source("code", code_file)
+    result_path = os.path.join(params.OUTPUT_DIR, result_file)
+
+    solution = computing.run_dummy2_task(None,
+                                        subtask_data,
+                                        difficulty,
+                                        result_size)
+
+    # TODO try catch and log errors. Issue #2425
+    with open(result_path, "w") as f:
+        f.write("{}".format(solution))
+
+
+run(params.data_files,
+    params.subtask_data,
+    params.difficulty,
+    params.result_size,
+    params.result_file)
---
+++ apps/dummy2/task/verifier.py
@@ -0,0 +1,48 @@
+import os
+from typing import Callable, Dict, Optional, Any
+import hashlib
+
+from golem_verificator.core_verifier import CoreVerifier
+from apps.dummy2.resources.code_dir import computing
+
+class Dummy2TaskVerifier(CoreVerifier):
+    # subtask_info is what sits in the task.subtasks_given["subtask_id"]
+    # it is set in the query_extra_data
+    def __init__(self, callback: Callable,
+                 verification_data: Optional[Dict[str, Any]] = None) -> None:
+        super().__init__(callback)
+        if verification_data:
+            self.subtask_info = verification_data["subtask_info"]
+        else:
+            self.subtask_info = None
+
+    def _verify_result(self, results: Dict[str, Any]):
+        
+        subtask_info = results["subtask_info"]
+        result = results["results"][0]
+
+        #Accessing owner task is required because it keeps randomly generated
+        #artificial passwords for each subtask
+        parent_task = subtask_info['parent_task']
+        subtask_offset = subtask_info['start_task'] - 1
+ 
+        original_passwds = set(parent_task.get_passwds_for_bucket(subtask_offset))
+
+        with open(result, 'r') as f:
+            result_passwds = set(f.read().split())
+
+        #Check mismatch reason
+        if not original_passwds == result_passwds:
+            #Mismatch subset must be a single element set containing the password of interest
+            #otherwise result is malicious
+            mismatch_subset = result_passwds.difference(original_passwds)
+            if len(mismatch_subset) != 1:
+                return False
+            realpasswd = mismatch_subset.pop()
+            sha = hashlib.sha256()
+            sha.update(realpasswd.encode())
+            if sha.hexdigest() != parent_task.password_hash:
+                return False
+            parent_task.real_password = realpasswd
+
+        return True
